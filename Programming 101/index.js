3624eba9e511e8fabdb7537387e0ed5f0d1b9b18
vijaychauhan <vijaychauhan88257@gmail.com>
2025-08-27 22:47:56 +0530

    Programming 101

---
+++ Programming 101/index.js
@@ -0,0 +1,28 @@
+console.log("Param Singh");// to print something on console
+// "" denotes string (a data type);
+// ; ends a statement
+
+// 7 this is a number but if we wrap this inside then "7" it becomes a string
+
+console.log(7 + 7); // addition of two numbers
+
+const a = 10; // assigning a value to a variable
+const b = 20;
+const multiply = a * b; // multiplication of two variables
+console.log(multiply);
+console.log(a + b); // addition of two variables
+
+
+const name = "Param"; // string variable
+//  we can't change the value of a constant variable
+// name = "Singh"; // it will throw an error
+
+let age = 21; // let is used to declare a variable whose value can be changed
+age = 22; // changing the value of age variable
+console.log(age);
+
+
+let firstName = "Param"; // string variable
+let lastName = "Singh"; // string variable
+let fullName = firstName + " " + lastName; // concatenation of two strings
+console.log(fullName);
